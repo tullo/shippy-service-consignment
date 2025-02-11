c70d710ff7743eb00cf146bcef552cf3289d6ab9
Andreas <704542+tullo@users.noreply.github.com>
2025-02-11 22:24:53 +0100

    Update repository.go

--- repository.go
+++ repository.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 
-	"github.com/micro/micro/v3/service/logger"
+	"github.com/micro/micro/v5/service/logger"
 	proto "github.com/tullo/shippy-service-consignment/proto"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
